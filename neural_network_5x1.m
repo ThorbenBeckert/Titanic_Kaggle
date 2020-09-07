cf5f6c6bdf88332d3faf60245796cf8bf85bb35b
ThorbenBeckert <ThorbenBeckert@users.noreply.github.com>
2020-09-07 17:14:58 +0200

    some corrections and preparations

---
+++ neural_network_5x1.m
@@ -0,0 +1,9 @@
+%% Initialization
+clear ; close all; clc
+
+%% Setup the parameters for the problem
+input_layer_size  = 0;		%will be changed after preparing the data
+num_layers = 2;				%1x5 Neural Network
+hidden_layer_size = 5;
+num_labels = 2;
+
